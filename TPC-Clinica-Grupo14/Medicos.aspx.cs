81a7db884b46c856aaed041d215cc0f185ff00bd
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2024-07-07 07:42:45 -0300

    se agrega formulario, gridview, botones agregar, modificar, eliminar, cancelar, en la pagina medico.aspx, sin funcionalidad por ahora , continuamos...

--- TPC-Clinica-Grupo14/Medicos.aspx.cs
+++ TPC-Clinica-Grupo14/Medicos.aspx.cs
@@ -12,14 +12,34 @@ namespace TPC_Clinica_Grupo14
     public partial class Medicos : System.Web.UI.Page
     {
         private ProfesionalNegocio profesionalNegocio = new ProfesionalNegocio();
+        private EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 cargarListaProfesionales();
+                cargarListaEspecialidades();
+                
 
             }
+        }
+
+        protected void cargarListaEspecialidades()
+        {
+            try
+            {
+                List<Especialidad> Lista = especialidadNegocio.Listar();
+                ddlEspecialidad.DataSource = Lista;
+                ddlEspecialidad.DataTextField = "Nombre";
+                ddlEspecialidad.DataValueField = "Id";
+                ddlEspecialidad.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar la lista de Especialidades: " + ex.Message);
+            }
+
         }
 
         protected void cargarListaProfesionales()
@@ -35,6 +55,54 @@ namespace TPC_Clinica_Grupo14
                 Console.WriteLine("Error al cargar la lista de Medicos: " + ex.Message);
             }
 
+        }
+
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+
+        }
+
+
+        protected void dgvMedicos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+
+
+
+        private void LimpiarCampos()
+        {
+            txtId.Text = "";
+            txtIdProfesional.Text = "";
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtFechaNacimiento.Text = "";
+            txtDNI.Text = "";
+            txtCorreo.Text = "";
+            txtTelefono.Text = "";
+            txtActivo.SelectedIndex = 0;
+            ddlEspecialidad.SelectedIndex = 0;
         }
     }
 }
